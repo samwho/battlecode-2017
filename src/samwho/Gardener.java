38e95fe5fa7b3d06604c0070907695cecb2dffa4
Sam Rose <samwho@lbak.co.uk>
2017-02-19 20:17:16 +0000

    Document gardener behaviour.

--- src/samwho/Gardener.java
+++ src/samwho/Gardener.java
@@ -45,6 +45,11 @@ public strictfp class Gardener extends Robot {
       debug_out("planting trees...");
 
       List<MapLocation> ls = getSurroundingLocations(toPlant + 1, distance);
+
+      // Because we might not be able to plant all of our trees on the first
+      // pass (things might be in the way, or we might not be able to afford it,
+      // or we might have build cooldowns), we wrap this in a while(true) that
+      // breaks after we've planted 5 trees.
       while (true) {
         for (MapLocation l : ls) {
           Direction d = rc.getLocation().directionTo(l);
@@ -70,11 +75,19 @@ public strictfp class Gardener extends Robot {
   private GameRunnable moveToGardeningLocation() {
     return () -> {
       while (true) {
+        // We're going to go with a hexagonal planting strategy. The idea behind
+        // this is to plant 5 trees around us and then leave a gap for spawning
+        // soldiers. As a result, the only condition for a gardening location at
+        // the moment is enough space for our gardener and his surrounding
+        // trees.
         if (!rc.isCircleOccupiedExceptByThisRobot(rc.getLocation(), 3)) {
           debug_out("found gardening location!");
           break;
         }
 
+        // TODO(samwho): Improve pathing here. At the moment we've just settled
+        // on randomly moving around until we stumble upon a valid gardening
+        // location.
         while (!rc.hasMoved()) {
           debug_out("moving to find gardening location");
           tryMove(randomDirection());
@@ -83,6 +96,7 @@ public strictfp class Gardener extends Robot {
     };
   }
 
+  // Returns the lowest health tree in the immediate vicinity of this gardener.
   private TreeInfo getSaddestNearbyTree() throws GameActionException {
     float treeRadius = 1.0f;
     float distance = rc.getType().bodyRadius + 0.01f + treeRadius;
@@ -92,6 +106,8 @@ public strictfp class Gardener extends Robot {
       return null;
     }
 
+    // TODO(samwho): There's probably a nicer thing we can do here with
+    // Collections.min and lambdas, but I couldn't figure it out.
     TreeInfo saddest = nearbyTrees[0];
     for (int i = 0; i < nearbyTrees.length; i++) {
       if (nearbyTrees[i].health < saddest.health) {
