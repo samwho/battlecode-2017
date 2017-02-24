fedaf32536c3341be3d17517f6e4595d95b6d296
Sam Rose <samwho@lbak.co.uk>
2017-02-24 13:30:48 +0000

    Refactor RobotFactory to allow me to create new 'strategies'

--- src/samwho/RobotFactory.java
+++ src/samwho/RobotFactory.java
@@ -4,36 +4,44 @@ import samwho.robots.*;
 
 import battlecode.common.*;
 
+import java.util.function.Supplier;
+import java.util.HashMap;
+import java.util.Map;
+
 public strictfp class RobotFactory {
+  // The default strategy. Eventually I'll branch out into trying more
+  // strategies, but for the time being this is it.
+  static final Map<RobotType, Supplier<Robot>> DEFAULT_STRATEGY =
+    new HashMap<RobotType, Supplier<Robot>>() {{
+
+    put(RobotType.ARCHON,     () -> new Archon());
+    put(RobotType.GARDENER,   () -> new Gardener());
+    put(RobotType.SOLDIER,    () -> new Soldier());
+    put(RobotType.LUMBERJACK, () -> new Lumberjack());
+  }};
+
+  /**
+   * A "strategy" is, for the time being, simply a map of RobotType to Robot
+   * supplier that will implement the behaviour for that type.
+   *
+   * A known limitation of this is that you can't switch a robot's type mid-way
+   * through a game.
+   */
+  static final Map<RobotType, Supplier<Robot>> STRATEGY = DEFAULT_STRATEGY;
+
   public static Robot create(RobotController rc) {
-    Robot r;
+    Supplier<Robot> s = STRATEGY.get(rc.getType());
 
-    switch (rc.getType()) {
-      case ARCHON:
-        r = new Archon();
-        break;
-      case GARDENER:
-        r = new Gardener();
-        break;
-      case SOLDIER:
-        r = new Soldier();
-        break;
-      case TANK:
-        r = null;
-        break;
-      case SCOUT:
-        r = null;
-        break;
-      case LUMBERJACK:
-        r = new Lumberjack();
-        break;
-      default:
-        r = null;
+    // No mapping for this robot type. Handle that further up the stack.
+    if (s == null) {
+      return null;
     }
 
-    if (r != null) {
-      r.setRobotController(rc);
-    }
+    Robot r = s.get();
+
+    // Always be sure to pass the robot its RobotController, otherwise it won't
+    // be able to do anything meaningful.
+    r.setRobotController(rc);
 
     return r;
   }
